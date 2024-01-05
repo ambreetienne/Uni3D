cc6990eb1ddfb19028f9b0d019ccfcf6b913faf7
ambreetienne <ambretienne@gmail.com>
2024-01-05 19:33:20 +0100

    plot

--- partial_point.py
+++ partial_point.py
@@ -586,7 +586,7 @@ def test_zeroshot_3d_core(test_loader, validate_dataset_name, model, clip_model,
                 progress.display(i)
 
         np.save('results/partial_points/results_top_1', per_class_correct_top1)
-        np.save('results/partial_pointss/results_top_3', per_class_correct_top3)
+        np.save('results/partial_points/results_top_3', per_class_correct_top3)
         np.save('results/partial_points/results_top_5', per_class_correct_top5)
         np.save('results/partial_points/per_class_stats', per_class_stats)
         
